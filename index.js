a6254984f1e71ed671b4adf89d068e45ed3a6b01
vigneshkumarlakshmanan <102342407+vigneshkumarlakshmanan@users.noreply.github.com>
2025-11-06 10:59:30 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello CI/CD World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
