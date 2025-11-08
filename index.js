b43c22c6e024c56ef6a7de3984918b4675d06cc4
vigneshkumarlakshmanan <102342407+vigneshkumarlakshmanan@users.noreply.github.com>
2025-11-08 01:04:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD World!....');
+  res.send('Hello CI/CD World!.');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
